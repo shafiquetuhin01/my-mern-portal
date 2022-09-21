40f69309a0dd7d8e87e3a8a613b051f8fa042378
unknown <sfqtuhin04@gmail.com>
2022-09-21 17:52:24 +0600

    module 77 updating

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,70 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
-import { useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { Link, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
 
 const MyAppointments = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
-    const [appointments, setAppointments] = useState([]);
-    const [user] = useAuthState(auth);
-    const navigate = useNavigate()
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?patient=${user.email}`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => {
+          console.log("res", res);
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem("accessToken");
+            navigate("/");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setAppointments(data);
+        });
+    }
+  }, [user]);
 
-    useEffect(() => {
-        if (user) {
-            fetch(`http://localhost:5000/booking?patient=${user.email}`, {
-                method: 'GET',
-                headers: {
-                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
-                .then(res => {
-                    console.log('res', res);
-                    if (res.status === 401 || res.status === 403) {
-                        signOut(auth);
-                        localStorage.removeItem('accessToken');
-                        navigate('/');
-                    }
-                    return res.json()
-                })
-                .then(data => {
-
-                    setAppointments(data);
-                });
-        }
-    }, [user])
-
-    return (
-        <div>
-            <h2>My Appointments: {appointments.length}</h2>
-            <div className="overflow-x-auto">
-                <table className="table w-full">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Date</th>
-                            <th>Time</th>
-                            <th>Treatment</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            appointments.map((a, index) => <tr>
-                                <th>{index + 1}</th>
-                                <td>{a.patientName}</td>
-                                <td>{a.date}</td>
-                                <td>{a.slot}</td>
-                                <td>{a.treatment}</td>
-                            </tr>)
-                        }
-
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2>My Appointments: {appointments.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment</th>
+              <th>Payment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments.map((a, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>{a.patientName}</td>
+                <td>{a.date}</td>
+                <td>{a.slot}</td>
+                <td>{a.treatment}</td>
+                <td>
+                    {(a.price && !a.paid) && <Link to={`/dashboard/payment/${a._id}`}><button className="btn-xs btn btn-success">Pay</button></Link>}
+                    {(a.price && a.paid) && <span className="text-success">Paid</span>}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
